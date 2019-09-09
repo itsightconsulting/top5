5abb09e261dd4f08438d811549b7423430c17836
Diego Herrera <diego.herrera@itsight.pe>
2019-09-08 22:36:38 -0500

    DH models

--- src/database/config.js
+++ src/database/config.js
@@ -1,4 +1,5 @@
-export const database = {
+
+const dev = {
     host: '52.186.67.112',
     port: '5432',
     user: 'postgres',
@@ -6,3 +7,13 @@ export const database = {
     databaseName: 'top5_desa',
     dialect: 'postgres'
 };
+
+const local = {
+    host: 'localhost',
+    port: '5432',
+    user: 'postgres',
+    password: 'postgresql',
+    databaseName: 'top5_desa',
+    dialect: 'postgres'
+};
+export const database = local;
