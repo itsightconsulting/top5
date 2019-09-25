fa5f174f649d85be82835e949aaafce5bc249cd6
Diego Herrera <diego.herrera@itsight.pe>
2019-09-25 18:03:37 -0500

    ignore constants

--- src/utilitarios/constants.js
+++ src/utilitarios/constants.js
@@ -1,5 +1,5 @@
-const ACCESS_KEY_ID = "";
-const SECRET_ACCESS_KEY = "";
+const ACCESS_KEY_ID = "AKIAYNGTFJVSK42YP4VF";
+const SECRET_ACCESS_KEY = "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";
 const aws_config_s3 = { ACCESS_KEY_ID, SECRET_ACCESS_KEY };
 
 module.exports = {
